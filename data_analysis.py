a45f7558d808c8b9f9ce697fde33477a803c79df
kennethbollen <31512761+kennethbollen@users.noreply.github.com>
2018-03-19 10:39:05 +0000

    Update data_analysis.py
    fixed indentation level for diff means

--- data_analysis.py
+++ data_analysis.py
@@ -41,9 +41,8 @@ def draw_perm_reps(data_1, data_2, func, size=1):
 
     return perm_replicates
   
- def diff_of_means(data_1, data_2):
+def diff_of_means(data_1, data_2):
     """Difference in means of two arrays."""
-
     # The difference of means of data_1, data_2: diff
     diff = np.mean(data_1) - np.mean(data_2)
 

9bb090d5782c3f43d3d67cfe93e246cfa15d3ebe
kennethbollen <31512761+kennethbollen@users.noreply.github.com>
2018-04-14 15:53:50 +0100

    Update eda_2.py

--- eda_2.py
+++ eda_2.py
@@ -1,4 +1,4 @@
-from data_extract import *
+#from data_extract import *
 import pandas as pd
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -84,9 +84,11 @@ pub_yrs_mean = pub_yrs_mean.fillna(value=0)
 pub_yrs_cnt = pub_yrs_cnt.sort_values(by='total_5yrs', ascending=False)
 pub_yrs_mean = pub_yrs_mean.sort_values(by='total_5yrs', ascending=False)
 #calculate the percentage changes by the year
+pub_yrs_cnt['pct_chg_14'] = (pub_yrs_cnt['yr_2014'] - pub_yrs_cnt['yr_2013']) / pub_yrs_cnt['yr_2013']
 pub_yrs_cnt['pct_chg_15'] = (pub_yrs_cnt['yr_2015'] - pub_yrs_cnt['yr_2014']) / pub_yrs_cnt['yr_2014']
 pub_yrs_cnt['pct_chg_16'] = (pub_yrs_cnt['yr_2016'] - pub_yrs_cnt['yr_2015']) / pub_yrs_cnt['yr_2015']
 pub_yrs_cnt['pct_chg_17'] = (pub_yrs_cnt['yr_2017'] - pub_yrs_cnt['yr_2016']) / pub_yrs_cnt['yr_2016']
+pub_yrs_mean['pct_mean_chg_14'] = (pub_yrs_mean['yr_2014'] - pub_yrs_mean['yr_2013']) / pub_yrs_mean['yr_2013']
 pub_yrs_mean['pct_mean_chg_15'] = (pub_yrs_mean['yr_2015'] - pub_yrs_mean['yr_2014']) / pub_yrs_mean['yr_2014']
 pub_yrs_mean['pct_mean_chg_16'] = (pub_yrs_mean['yr_2016'] - pub_yrs_mean['yr_2015']) / pub_yrs_mean['yr_2015']
 pub_yrs_mean['pct_mean_chg_17'] = (pub_yrs_mean['yr_2017'] - pub_yrs_mean['yr_2016']) / pub_yrs_mean['yr_2016']
@@ -101,12 +103,105 @@ books_date_non_fiction = df_books.loc[df_books['Type'] == 'non_fiction',:].group
 books_date_fiction = books_date_fiction.resample('A').mean()
 books_date_non_fiction = books_date_non_fiction.resample('A').mean()
 
-#plot line graphs
-plt.plot(books_date_fiction, linestyle='-', marker='o', color='r', linewidth=1.0)
-plt.plot(books_date_non_fiction, linestyle='-', marker='o', color='b', linewidth=1.0)
-plt.xlabel('2013 - 2018')
-plt.ylabel('Averager User Ratings')
-plt.title('NY Times Best Seller Books Average User Ratings')
+def autolabel(rects):
+    """
+    Attach a text label above each bar displaying its height
+    """
+    for rect in rects:
+        height = rect.get_height()
+        ax.text(rect.get_x() + rect.get_width()/2., 1.05*height,
+                '%d' % int(height),
+                ha='center', va='bottom')
+
+def autolabel_plt(rects):
+    """
+    Attach a text label above each bar displaying its height
+    """
+    for rect in rects:
+        height = rect.get_height()
+        plt.text(rect.get_x() + rect.get_width()/2., 1.05*height,
+                '%d' % int(height),
+                ha='center', va='bottom')
+
+
+#who are the top 10 publishers by number of best sellers?
+top_10_pub = pub_yrs_cnt.head(10)   
+bar_xlabel = []
+
+for i in top_10_pub.index:
+	bar_xlabel.append(i)
+
+N = len(top_10_pub)
+width = 0.35        
+top_10 = plt.bar(bar_xlabel, top_10_pub['total_5yrs'], width, color='r')
+plt.xlabel('Publishers')
+plt.ylabel('Number of Best Sellers')
+plt.title('Top 10 Publishers based on #of Best Sellers (last 5 years)')
+plt.show()
+
+
+#what is the difference between the number of best sellers in 2013 and 2017
+top_10_delta = pd.Series((top_10_pub['yr_2017'] - top_10_pub['yr_2013'])/top_10_pub['yr_2013'])
+
+ind = np.arange(N)
+fig, ax = plt.subplots()
+y13 = ax.bar(ind, top_10_pub['yr_2013'], width, color='r')
+y17 = ax.bar(ind + width, split_test['yr_2017'], width, color='y')
+ax.set_ylabel('Number of NY Times Best Sellers')
+ax.set_xlabel('Publishers (2013 vs 2017')
+ax.set_title('Change in Num of Best Sellers for Top 10 Publishers')
+ax.set_xticks(ind + width / 2)
+ax.set_xticklabels(bar_xlabel)
+
+ax2 = ax.twinx()
+line1 = ax2.plot(ax.get_xticks(), top_10_delta , linestyle='-', marker='o', color='b', linewidth=1.0)
+
+vals = ax2.get_yticks()
+ax2.set_yticklabels(['{:3.2f}%'.format(x*100) for x in vals])
+ax2.set_ylabel('% Change')
+
+ax.legend((rects1[0], rects2[0], line1[0]), ('2013', '2017', '% Change'))
+
+
+autolabel(y13)
+autolabel(y17)
+plt.show()
+
+#has this been a trend?
+trend = top_10_pub.loc[:,['pct_chg_14','pct_chg_15', 'pct_chg_16', 'pct_chg_17']]
+trend = trend.T
+trend_xlabels = ['2014', '2015', '2016', '2017']
+
+for i in top_10_pub.index:
+    plt.plot(trend_xlabels, trend[i], linestyle='-', marker='o')
+    
+plt.xlabel('Years')
+plt.ylabel('% Change')
+plt.title('Changes in # of Best Sellers')
 plt.legend(loc='best')
 plt.show()
+
+#has the quality of the books been going down?
+top_10_ratings = pd.DataFrame(top_10_pub.index)
+top_10_ratings = pd.merge(top_10_ratings, pub_yrs_mean, how='left', left_on='Publisher', right_index=True)
+top_10_ratings = top_10_ratings.set_index('Publisher')
+top_10_ratings = top_10_ratings[['yr_2013','yr_2014','yr_2015','yr_2016','yr_2017']]
+top_10_ratings = top_10_ratings.T
+
+ratings_xlabels = ['2013','2014','2015','2016','2017']
+N = len(ratings_xlabels)
+ind = np.arange(N)
+width = 0.35
+fig, ax = plt.subplots()
+ax.set_xticks(ind + width / 2)
+ax.set_xticklabels(rating_xlabel)
+
+for pub in top_10_ratings.columns:
+    ax.plot(ax.get_xticks(), top_10_ratings[pub], linestyle='-', marker='o')
+
+ax.set_xlabel('Years')
+ax.set_ylabel('Average User Ratings')
+ax.set_title('User Ratings for Publication Books over 5 years')
+ax.legend(loc='best')
+
 
